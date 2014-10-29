afe5ad02b52fd8db7ee69415e04e7d42ad93faf9
morning <1137929627@qq.com>
2014-10-29 21:53:19 +0800

    add /comment

--- xichao_wechat_web/xichao.py
+++ xichao_wechat_web/xichao.py
@@ -1,8 +1,8 @@
-#-*-coding:utf-8-*-
+# -*- coding:utf-8 -*-
 from cStringIO import StringIO
 import os
 import MySQLdb
-from flask import Flask, request,session, g, redirect, url_for,render_template,flash,abort
+from flask import Flask, request,session, g, redirect, url_for,render_template,flash
 from werkzeug import secure_filename
 from flask import send_from_directory
 from time import time
@@ -10,6 +10,10 @@ from flask_wtf.file import FileField
 #from sqlalchemy import *
 #import sqlalchemy.util as util
 import string, sys
+
+##reload(sys)
+##sys.setdefaultencoding('utf8')
+
 #from sqlalchemy.databases import mysql
  
 DEBUG = True
@@ -31,20 +35,10 @@ app.config.update(
     USERNAME = 'xichao',
     PASSWORD = 'xichao123'
     )
-f=open('test.txt','a')
-
-
-
-
-
-
-    
-
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
    
-
 @app.route("/")
 def index():
     return render_template('index.html')
@@ -75,7 +69,7 @@ def logout():
 
 @app.route("/test/") 
 def test():
-    conn = MySQLdb.connect(host='localhost', user='root',passwd='') 
+    conn = MySQLdb.connect(host='localhost', user='root',passwd='1234') 
     conn.select_db('xichao_wechat');
     cursor = conn.cursor(cursorclass=MySQLdb.cursors.DictCursor)
     x=cursor.execute("select image_path,description from xichao_theme order by tid DESC limit 100")
@@ -142,10 +136,10 @@ def upload_file():
                 return '<script type="text/javascript" >alert("uploaded!");</script>'
         return render_template("upload.html",maxtid=maxtid)
     else:
-        abort(403,"permission denied")
+        return '<h1>permission denied</h1>'
 
 
-@app.route('/display', methods=['GET', 'POST'])
+@app.route('/display')
 def uploaded():
     conn = MySQLdb.connect(host='localhost', user='root',passwd='') 
     conn.select_db('xichao_wechat');
@@ -165,8 +159,43 @@ def uploaded():
         text=data[4]
     
     return render_template('display.html',tid=tid,image_path=image_path,description=description,title=title,text=text)   
+@app.route('/comment', methods=['GET', 'POST'])
+def comment():
+    conn = MySQLdb.connect(host='localhost', user='root',passwd='1234') 
+    conn.select_db('xichao_wechat');
+    cursor = conn.cursor()
+    cursor.execute("select * from xichao_comments order by tid")
+    data=[]
+    while True:
+        try:
+            data+=[cursor.fetchone()[1]]
+        except:
+            break
+    cursor.execute("select count(*) as value from xichao_comments ")
+    num=cursor.fetchone()[0]
+    num=int(num)
+    cm=''
+    if request.method == 'POST':       
+        cm=request.form['text']
+        if cm!='':
+            order=(num+1,cm)
+            sql = "insert into xichao_comments(tid,comment) values (%s,%s)"
+            cursor.execute(sql,order)
+            num+=1
+    s=''
+    cur=1
+    for i in data:
+        s+=str(cur)+'.'+i+'d%2S'
+        cur+=1
+    if cm!='':
+        s+=str(cur)+'.'+cm+'d%2S'
+    s=s.rstrip('d%2S')
+    conn.commit()
+    cursor.close() 
+    conn.close()   
+        
+    return render_template("comment.html",comment=s,number=num)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
