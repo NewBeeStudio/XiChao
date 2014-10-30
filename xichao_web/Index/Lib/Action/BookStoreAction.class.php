71b369a96abdbc811d455391294e919f2ce94f2c
朱亮 <torres@zhuliangdeMacBook-Air.local>
2014-10-31 00:46:11 +0800

    add new files and folders about module of 'xichao bookstore'

---
+++ xichao_web/Index/Lib/Action/BookStoreAction.class.php
@@ -0,0 +1,10 @@
+<?php
+
+//曦潮书店
+
+class BookStoreAction extends Action{
+	//曦潮书店界面显示
+	Public function index(){
+		$this->display();
+	}
+}
---
+++ xichao_web/Index/Lib/Action/ServiceAction.class.php
@@ -0,0 +1,10 @@
+<?php
+
+//曦潮服务
+
+class ServiceAction extends Action{
+	//曦潮服务界面显示
+	Public function index(){
+		$this->display();
+	}
+}
