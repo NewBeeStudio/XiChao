abfd925fc3db1b90bb9cef11ee2a998398c99f12
朱亮 <torres@zhuliangdeMacBook-Air.local>
2014-11-01 11:34:08 +0800

    modify the module of register

--- xichao_web/Index/Runtime/Cache/3ee7c294932736e1750edd280ffd47d0.php
+++ xichao_web/Index/Runtime/Cache/3ee7c294932736e1750edd280ffd47d0.php
@@ -10,6 +10,7 @@
     
     <link rel="stylesheet" type="text/css" href="__PUBLIC__/Css/register.css" />
     <link rel="stylesheet" type="text/css" href="__PUBLIC__/Css/header.css" />
+    <link rel="stylesheet" type="text/css" href="__PUBLIC__/Css/footer.css" />
 
     <script src="__PUBLIC__/Js/jquery.min.js"></script>
     <script src="__PUBLIC__/Js/register.js"></script>
@@ -21,7 +22,25 @@
 <body>
 
 	<div class="container">
-		<div class="main_page_new">
+		<div class="main_page">
+<!--###################################header###################################-->
+            <!--###################################header###################################-->
+<div class="head">
+    <a class="login" href="__APP__/Login/index">登陆</a>
+    <a class="register" href="__APP__/Register/index">注册</a>
+    <a class="logo" href="__APP__/Index/index"><img height="75px" src="__PUBLIC__/Images/rt_logo.png"></a>
+</div>   
+<div class="nav_row">
+    <hr class="up">
+    <a id="header_item0" href="__APP__/Index/index">首页</a>
+    <a id="header_item1" href="__APP__/BookStore/index">曦潮书店</a>
+    <a id="header_item2" href="#">会员</a>
+    <a id="header_item3" href="#">人文</a>
+    <a id="header_item4" href="#">活动</a>
+    <a id="header_item5" href="__APP__/Recruit/index">招募</a>
+    <hr class="down">
+</div>  
+
 			<div id="register_header">
 				<img src="__PUBLIC__/Images/myXichao.png" style="position:absolute">
 				<p id="register_title">账户注册</P>
@@ -94,9 +113,23 @@
 					-->
 				</form>
 			</div>
+<!--###################################footer###################################--> 
+        	<!--###################################footer###################################-->
+<hr>
+<div class="footer-body">
+    <a id="footer_item1" href="#">关于我们</a>
+    <a id="footer_item2" href="#" >联系我们</a>
+    <a id="footer_item3" href="#" >加入我们</a>
+    <a id="footer_item4" href="#" >服务声明</a>
+</div>   
+<div id="copyright">
+    <div><b>Copyright©曦潮2014，All Right Reserved|沪ICP备xxxxxx号</b>
+    </div>  
+</div>   
+
 		</div>
 	</div>
-
+</body>
 <script>
 	function show(obj){
 		obj.src="__APP__/Common/verify/random/"+Math.random();
