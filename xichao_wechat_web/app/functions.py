98ae27e0418b4ab3df060c8fc3333f1962440ea4
lxserenade <lixia625@hotmail.com>
2015-02-12 15:15:35 +0800

    path

--- xichao_wechat_web/app/functions.py
+++ xichao_wechat_web/app/functions.py
@@ -11,7 +11,7 @@ def datatofile(data):
 	imgdata=data.split(';')[1].split(',')[1]
 	imgData = base64.b64decode(imgdata)
 	filename=str(int(time.time()))
-	leniyimg = open(os.path.join('/home/newbee/git/XiChao/xichao_wechat_web/app/',UPLOAD_FOLDER)+filename+"."+imgtype,'w')
+	leniyimg = open(os.path.join('/home/xichao/git/XiChao/xichao_wechat_web/app/',UPLOAD_FOLDER)+filename+"."+imgtype,'w')
 	leniyimg.write(imgData)
 	leniyimg.close()
 	return filename+"."+imgtype
