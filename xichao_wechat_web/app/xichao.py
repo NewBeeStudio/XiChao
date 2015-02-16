16905dc4c2695a6f4d2c48cb0397a4a0c5f7fc88
lxserenade <lixia625@hotmail.com>
2015-02-16 15:57:29 +0800

    wx

--- xichao_wechat_web/app/xichao.py
+++ xichao_wechat_web/app/xichao.py
@@ -288,7 +288,7 @@ def upload():
         filename = secure_filename(tmpfilename)
                 # upfile 为 FileStorage 对象
         # 这里保存文件并返回相应的URL
-        upfile.save(os.path.join('/home/xichao/git/XiChao/xichao_wechat_web/app/',UPLOAD_FOLDER), filename))
+        upfile.save(os.path.join('/home/xichao/git/XiChao/xichao_wechat_web/app/',UPLOAD_FOLDER), filename)
         result = {
             "state": "SUCCESS",
             "url": HOST+"/mobile/list/static/upload_images/"+filename,
